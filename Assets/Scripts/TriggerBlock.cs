6dfdf3470635ee1b7130c0c7818cb3a989c1efb7
beomjoon <tk2010kbj@naver.com>
2022-06-30 20:23:37 +0900

    Power, Portal 세팅 구현 Block 우클릭으로 로그 출력

--- Assets/Scripts/TriggerBlock.cs
+++ Assets/Scripts/TriggerBlock.cs
@@ -9,13 +9,44 @@ public class TriggerBlock : MonoBehaviour
 
     Vector2Int idx;
 
+    bool isMouseEntered;
+    bool isMouseRightClicked;
+    Vector3 lastMouseRightClickedPos;
+
     private void Awake()
     {
         drawLayer = GameObject.Find("DrawLayer").GetComponent<DrawLayer>();
+
+        isMouseEntered = false;
+        isMouseRightClicked = false;
+        lastMouseRightClickedPos = Vector3.zero;
+    }
+
+    public void Update()
+    {
+        if (isMouseEntered && Input.GetMouseButtonDown(1))
+        {
+            isMouseRightClicked = true;
+            lastMouseRightClickedPos = Input.mousePosition;
+        }
+
+        if (isMouseRightClicked)
+        {
+            if (lastMouseRightClickedPos != Input.mousePosition)
+            {
+                isMouseRightClicked = false;
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                Debug.Log("타일정보창 열기");
+            }
+        }
     }
 
     private void OnMouseEnter()
     {
+        isMouseEntered = true;
+
         if (Input.GetMouseButton(0))
         {
             if (!EventSystem.current.IsPointerOverGameObject())
@@ -31,6 +62,8 @@ public class TriggerBlock : MonoBehaviour
 
     private void OnMouseExit()
     {
+        isMouseEntered = false;
+        isMouseRightClicked = false;
         drawLayer.TriggerMouseExit(idx);
     }
 
